ca15c29bf241ef679557c7536c97168ae204d71f
Erwin Janssen <erwinjanssen@outlook.com>
2019-07-03 09:48:14 +0200

    Add --commit flag to update_repo script

--- update_repository.py
+++ update_repository.py
@@ -1,14 +1,17 @@
+import json
 import os
 import sys
-import json
 
 import click
 import gitlab
-from jinja2 import Template
 import requests
+from jinja2 import Template
+
 
 def query_docker_image_tags(image):
-    req = requests.get(f"https://registry.hub.docker.com/v1/repositories/tlsprint/{image}/tags")
+    req = requests.get(
+        f"https://registry.hub.docker.com/v1/repositories/tlsprint/{image}/tags"
+    )
     tag_info = json.loads(req.content.decode())
     return {info["name"] for info in tag_info}
 
@@ -31,37 +34,41 @@ def query_docker_image_tags(image):
 @click.option(
     "-v", "--verbose", is_flag=True, default=False, help="Provide verbose output"
 )
-def main(api_key, gitlab_url, project_id, verbose):
-    # Query environment variable if --api-key is not passed
-    if not api_key:
-        try:
-            api_key = os.environ["GITLAB_TLSPRINT_API_KEY"]
-        except KeyError:
-            print(
-                "No API key specified. Specify via --api-key or define GITLAB_TLSPRINT_API_KEY.",
-                file=sys.stderr,
-            )
-            sys.exit(1)
-
-    gl = gitlab.Gitlab(gitlab_url, private_token=api_key)
-    project = gl.projects.get(50)
-
+@click.option(
+    "--commit",
+    is_flag=True,
+    default=False,
+    help="Indicates whether the files modified by this script should be committed.",
+)
+def main(api_key, gitlab_url, project_id, verbose, commit):
     tags = query_docker_image_tags("openssl")
 
     # For now only use image > 1.0.1, these support all TLS12
-    targets = [{
-        "implementation": "openssl",
-        "version": version,
-        "supported_TLS": ["TLS12"],
-    } for version in tags if version > "1.0.1"]
-
+    targets = [
+        {"implementation": "openssl", "version": version, "supported_TLS": ["TLS12"]}
+        for version in tags
+        if version > "1.0.1"
+    ]
     with open(".drone.yml.j2") as f:
         template = Template(f.read())
 
     with open(".drone.yml", "w") as f:
         f.write(template.render(targets=targets))
 
+    if commit:
+        # Query environment variable if --api-key is not passed
+        if not api_key:
+            try:
+                api_key = os.environ["GITLAB_TLSPRINT_API_KEY"]
+            except KeyError:
+                print(
+                    "No API key specified. Specify via --api-key or define GITLAB_TLSPRINT_API_KEY.",
+                    file=sys.stderr,
+                )
+                sys.exit(1)
 
+        gl = gitlab.Gitlab(gitlab_url, private_token=api_key)
+        project = gl.projects.get(project_id)
 
 
 if __name__ == "__main__":
